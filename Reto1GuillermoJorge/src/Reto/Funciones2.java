3a7bd17c64520d38802318ccddba0715e1628682
guillemdb1994-pixel <Guille@Laptop-GMB>
2025-11-17 12:59:27 +0100

    qweqwe

--- Reto1GuillermoJorge/src/Reto/Funciones2.java
+++ Reto1GuillermoJorge/src/Reto/Funciones2.java
@@ -1,5 +1,22 @@
 package Reto;
-
+//Guillermo
 public class Funciones2 {
-
+	public static double multiplicar(double x, int y) {
+			return x*y;
+	}
+	public static double dividir(double x,double y) {
+		return x/y;
+	}
+	public static boolean esPositivo(double x) {
+		if (x>=0) {
+			return true;
+		}
+		return false;
+	}
+	public boolean esNegativo(double x) {
+		if (x<0) {
+			return true;
+		}
+		return false;
+	}
 }
