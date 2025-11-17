046ead327ec1931c2037088fae0e5836cfab7e35
MegaGiorgio45 <Jorge@PO-JORGE>
2025-11-17 12:57:28 +0100

    operaciones básicas (sumar, restar) y de los métodos de verificación de propiedades de números (esPar y esImpar).

--- Reto1GuillermoJorge/src/Reto/Reto1.java
+++ Reto1GuillermoJorge/src/Reto/Reto1.java
@@ -3,7 +3,7 @@ package Reto;
 public class Reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub qwehjqiusdhiuashdiuqweuiqwiue
+		// TODO Auto-generated method stub 
 
 	}
 
